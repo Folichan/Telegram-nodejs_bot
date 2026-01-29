8d1b59f7d99610f1a1a18d6fc5a5f6eb2e7b6f7c
Сергей <olga.t.38rus@gmal.com>
2026-01-29 11:55:17 +0800

    Telegram bot working with MySQL database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const db = require('./db');
 const TelegramBot = require('node-telegram-bot-api');
 
 // CREATE BOT
@@ -10,6 +11,16 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(chatId, 'Привет, Октагон!');
 });
 
+bot.setMyCommands([
+  { command: 'help', description: 'Список команд' },
+  { command: 'site', description: 'Сайт Октагона' },
+  { command: 'creator', description: 'Создатель бота' },
+  { command: 'randomitem', description: 'Случайный предмет' },
+  { command: 'getitembyid', description: 'Получить предмет по ID' },
+  { command: 'deleteitem', description: 'Удалить предмет по ID' }
+]);
+
+
 // /help
 bot.onText(/\/help/, (msg) => {
   const chatId = msg.chat.id;
@@ -18,6 +29,9 @@ bot.onText(/\/help/, (msg) => {
 Список команд:
 /site — ссылка на сайт Октагон;
 /creator — создатель бота!
+/randomitem — случайный предмет;
+/getitembyid — получение предмета по ID;
+/deleteitem — удаление предмета по ID.
 `;
 
   bot.sendMessage(chatId, text);
@@ -34,5 +48,73 @@ bot.onText(/\/creator/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Бота создала Тарасова Ольга Сергеевна!');
 });
+
+// /randomitem
+bot.onText(/\/randomitem/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const sql = `
+    SELECT id, name, \`desc\`
+    FROM items
+    ORDER BY RAND()
+    LIMIT 1
+  `;
+
+  db.query(sql, (err, results) => {
+    if (err || results.length === 0) {
+      bot.sendMessage(chatId, 'Ошибка...');
+      return;
+    }
+
+    const item = results[0];
+    bot.sendMessage(
+      chatId,
+      `(${item.id}) - ${item.name}: ${item.desc}`
+    );
+  });
+});
+
+// /getitembyid N
+bot.onText(/\/getitembyid (\d+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const id = match[1];
+
+  const sql = 'SELECT id, name, `desc` FROM items WHERE id = ?';
+
+  db.query(sql, [id], (err, results) => {
+    if (err || results.length === 0) {
+      bot.sendMessage(chatId, 'Ошибка...');
+      return;
+    }
+
+    const item = results[0];
+    bot.sendMessage(
+      chatId,
+      `(${item.id}) - ${item.name}: ${item.desc}`
+    );
+  });
+});
+
+//deleteitem N
+bot.onText(/\/deleteitem (\d+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const id = match[1];
+
+  const sql = 'DELETE FROM items WHERE id = ?';
+
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      bot.sendMessage(chatId, 'Ошибка...');
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      bot.sendMessage(chatId, 'Ошибка...');
+    } else {
+      bot.sendMessage(chatId, 'Удалено.');
+    }
+  });
+});
+
 
 console.log('Бот запущен.');
